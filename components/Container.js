e5dea77e0a870e3967f260bf7fed907baded396d
Wan Qing <leewanqing@trinax.sg>
2022-07-22 19:35:31 +0800

    add part 2

--- components/Container.js
+++ components/Container.js
@@ -18,6 +18,11 @@ const Container = ({ children }) => {
     dark: "gray.300",
   };
 
+  const color = {
+    light: "black",
+    dark: "white",
+  };
+
   const StickyNav = styled(Flex)`
     position: sticky;
     z-index: 10;
@@ -28,7 +33,21 @@ const Container = ({ children }) => {
 
   return (
     <>
-      <StickyNav>
+      <StickyNav
+        flexDirection='row'
+        justifyContent='space-between'
+        alignItems='center'
+        maxWidth='800px'
+        minWidth='356px'
+        width='100%'
+        bg={bgColor[colorMode]}
+        as='nav'
+        px={[2, 6, 6]}
+        py={2}
+        mt={8}
+        mb={[0, 0, 1]}
+        mx='auto'
+      >
         <Box>
           <Link href='/' passHref>
             <Button
@@ -53,6 +72,16 @@ const Container = ({ children }) => {
         </Box>
         <DarkModeSwitch />
       </StickyNav>
+      <Flex
+        as='main'
+        justifyContent='center'
+        bg={bgColor[colorMode]}
+        color={color[colorMode]}
+        p={[0, 4, 4]}
+        mt={[4, 8, 8]}
+      >
+        {children}
+      </Flex>
     </>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,42 @@ import {
   useColorMode,
 } from "@chakra-ui/react";
 import customTheme from "../styles/theme";
+import { Global, css } from "@emotion/react";
 
 import "../styles/globals.css";
+
+const GlobalStyle = ({ children }) => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <>
+      <Global>
+        styles=
+        {css`
+          ::selection {
+            background-color: #90cdf4;
+            color: #fefefe;
+          }
+          ::-moz-selection {
+            background: #ffb7b7;
+            color: #fefefe;
+          }
+          html {
+            min-width: 356px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: ${colorMode === "light" ? "white" : "#171717"};
+          }
+        `}
+      </Global>
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -16,7 +50,9 @@ function MyApp({ Component, pageProps }) {
           useSystemColorMode: true,
         }}
       >
-        <Component {...pageProps} />
+        <GlobalStyle>
+          <Component {...pageProps} />
+        </GlobalStyle>
       </ColorModeProvider>
     </ChakraProvider>
   );
